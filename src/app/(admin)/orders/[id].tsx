f39c424f19f5fa74ff2a09cc6032a68c1d9ac564
shafir360 <shafir360@gmail.com>
2024-08-01 01:22:09 +0100

    added order status select (admin)

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet,FlatList } from 'react-native'
+import { View, Text, StyleSheet,FlatList , Pressable} from 'react-native'
 import React from 'react'
 import OrderListItem from '@/src/components/OrderListItem'
 import { useLocalSearchParams } from 'expo-router'
@@ -6,6 +6,8 @@ import orders from '../../../../assets/data/orders';
 import CartListItem from '@/src/components/CartListItem';
 import { OrderItem } from '@/src/types';
 import OrderItemListItem from '@/src/components/OrderItemListItem';
+import { OrderStatusList } from '@/src/types';
+import Colors from '@/src/constants/Colors';
 
 
 const orderScreenDetail = () => {
@@ -27,6 +29,40 @@ const orderScreenDetail = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
+        ListFooterComponent={()=>(
+          <>
+      <Text style={{ fontWeight: 'bold' }}>Status</Text>
+      <View style={{ flexDirection: 'row', gap: 5 }}>
+        {OrderStatusList.map((status) => (
+          <Pressable
+            key={status}
+            onPress={() => console.warn('Update status')}
+            style={{
+              borderColor: Colors.light.tint,
+              borderWidth: 1,
+              padding: 10,
+              borderRadius: 5,
+              marginVertical: 10,
+              backgroundColor:
+                order.status === status
+                  ? Colors.light.tint
+                  : 'transparent',
+            }}
+          >
+            <Text
+              style={{
+                color:
+                  order.status === status ? 'white' : Colors.light.tint,
+              }}
+            >
+              {status}
+            </Text>
+          </Pressable>
+        ))}
+  </View>
+</>
+
+        )}
       />
     </View>
 
